0ea30369b1c7f893fd91ed8bf4b8aab5797bc635
Artyom Pugachev <artempugachev1@gmail.com>
2020-12-10 23:16:41 +0300

    add clock component

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="app">
+      <div className="app__clock">
+        <Clock />
+      </div>
     </div>
   );
 }
---
+++ clock/src/Clock.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Clock.css';
+
+
+function Clock() {
+    return (
+        <div className='clock'>
+            <h1>Clock</h1>
+        </div>
+    )
+}
+
+export default Clock
