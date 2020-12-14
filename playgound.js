9468c5d14e486edb1e215b9d6b8d83e9e2ffc2f1
Artyom Pugachev <artempugachev1@gmail.com>
2020-12-14 17:55:53 +0300

    playground

--- playgound.js
+++ playgound.js
@@ -5,3 +5,10 @@ function sayHi() {
 let hello = sayHi;
 
 hello();
+
+// A function is a value representing an “action”
+// Regular values like strings or numbers represent the data.
+
+// A function can be perceived as an action.
+
+// We can pass it between variables and run when we want.
